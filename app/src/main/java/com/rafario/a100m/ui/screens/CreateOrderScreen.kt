7fc9643bf850bec8dbadff8449b28a28c6b7d928
rafael.rio <rafa2000inator@gmail.com>
2026-06-05 12:37:25 +0200

    Update CreateOrderScreen.kt

--- app/src/main/java/com/rafario/a100m/ui/screens/CreateOrderScreen.kt
+++ app/src/main/java/com/rafario/a100m/ui/screens/CreateOrderScreen.kt
@@ -1,8 +1,8 @@
 package com.rafario.a100m.ui.screens
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Check
@@ -29,7 +30,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -45,8 +45,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import com.rafario.a100m.data.datasource.CatalogoDataSource
 import com.rafario.a100m.data.models.Bebida
@@ -173,7 +173,8 @@ fun CreateOrderScreen(
                     singleLine = true,
                     supportingText = {
                         Text(text = "Obligatorio para crear el pedido")
-                    }
+                    },
+                    keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words)
                 )
             }
 
@@ -235,7 +236,7 @@ fun CreateOrderScreen(
                                 )
                                 SectionDivider(index, bebidas.lastIndex)
                             }
-                    }
+                        }
                 }
             }
 
